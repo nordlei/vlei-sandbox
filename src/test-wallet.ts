f46514379f57121dbb03a7ab20feef12fcf9070e
Daniel Lenksjö <5889538+lenkan@users.noreply.github.com>
2024-10-03 10:56:10 +0200

    implement reproduction for an issue where the last member is late joining group (#2)

--- src/test-wallet.ts
+++ src/test-wallet.ts
@@ -1,4 +1,19 @@
-import { HabState, randomPasscode, ready, SignifyClient, Tier } from "signify-ts";
+import {
+  Algos,
+  d,
+  HabState,
+  messagize,
+  Operation,
+  randomPasscode,
+  ready,
+  Siger,
+  SignifyClient,
+  Tier,
+  randomNonce,
+  CreateRegistryArgs,
+} from "signify-ts";
+// } from "signify-ts-old"; // Use signify-ts-old if testing against KERIA 0.1.3
+import { sleep } from "./test-utils";
 
 const KERIA_HOSTNAME = process.env.KERIA_HOSTNAME ?? `localhost`;
 const KERIA_AGENT_URL = `http://${KERIA_HOSTNAME}:3901`;
@@ -15,6 +30,10 @@ function assertDefined<T>(obj: T | null): asserts obj is T {
 }
 
 export class TestWallet {
+  static randomNonce(): string {
+    return randomNonce();
+  }
+
   private _client: SignifyClient | null = null;
   private _identifier: HabState | null = null;
 
@@ -75,11 +94,11 @@ export class TestWallet {
       transferable: true,
     });
     const inceptOperation = await inceptResult.op();
-    await this.client.operations().wait(inceptOperation);
+    await this.wait(inceptOperation);
 
     const agentResult = await this.client.identifiers().addEndRole(alias, "agent", agentId);
     const agentOperation = await agentResult.op();
-    await this.client.operations().wait(agentOperation);
+    await this.wait(agentOperation);
 
     await this.refreshIdentifier();
   }
@@ -98,11 +117,204 @@ export class TestWallet {
 
   async resolveOobi(oobi: string, alias?: string) {
     const op2 = await this.client.oobis().resolve(oobi, alias);
-    await this.client.operations().wait(op2);
+    await this.wait(op2);
   }
 
   async queryKeyState(prefix: string) {
     const op = await this.client.keyStates().query(prefix);
-    await this.client.operations().wait(op);
+    await this.wait(op);
+  }
+
+  async createGroup(groupAlias: string, args: { smids: string[]; isith: number }) {
+    const mhab = this.identifier;
+    if (!mhab) {
+      throw new Error("No local identifier created");
+    }
+
+    const states = await Promise.all(
+      args.smids.map(async (member) => {
+        const result = await this.client.keyStates().get(member);
+        return result[0];
+      })
+    );
+
+    const res = await this.client.identifiers().create(groupAlias, {
+      algo: Algos.group,
+      isith: args.isith,
+      nsith: args.isith,
+      mhab,
+      states,
+      rstates: states,
+    });
+
+    const attachment = d(
+      messagize(
+        res.serder,
+        res.sigs.map((sig: string) => new Siger({ qb64: sig }))
+      )
+    ).substring(res.serder.size);
+
+    const embeds = {
+      icp: [res.serder, attachment],
+    };
+
+    await this.client
+      .exchanges()
+      .send(mhab.name, "multisig", mhab, "/multisig/icp", { smids: args.smids }, embeds, args.smids);
+
+    return await res.op();
+  }
+
+  private createSeal(hab: HabState) {
+    const habStateEvent = hab.state?.ee as { s: string; d: string };
+    const seal = [
+      "SealEvent",
+      {
+        i: hab["prefix"],
+        s: habStateEvent["s"],
+        d: habStateEvent["d"],
+      },
+    ];
+
+    return seal;
+  }
+
+  async listEndRoles(alias: string, role = "agent") {
+    const path = role !== undefined ? `/identifiers/${alias}/endroles/${role}` : `/identifiers/${alias}/endroles`;
+    const response: Response = await this.client.fetch(path, "GET", null);
+    if (!response.ok) throw new Error(await response.text());
+    const result = await response.json();
+    return result;
+  }
+
+  async configureGroupAgents(groupAlias: string, dt: string): Promise<Operation[]> {
+    const members = await this.client.identifiers().members(groupAlias);
+    const ops: Operation[] = [];
+
+    for (const { aid, ends } of members.signing) {
+      const [agentId] = Object.keys(ends.agent);
+
+      if (typeof agentId !== "string") {
+        throw new Error(`No agent id on member ${aid}`);
+      }
+
+      const op = await this.addEndRole(groupAlias, dt, agentId);
+      ops.push(op);
+    }
+
+    return ops;
+  }
+
+  async listOtherMembers(group: HabState) {
+    const recipients = await this.client
+      .identifiers()
+      .members(group.name)
+      .then((members) =>
+        members.signing.map((m: { aid: string }) => m.aid).filter((aid: string) => aid !== group.group?.mhab.prefix)
+      );
+
+    return recipients;
+  }
+
+  async addEndRole(groupAlias: string, timestamp: string, agentId: string) {
+    const hab = await this.client.identifiers().get(groupAlias);
+    const result = await this.client.identifiers().addEndRole(hab.name, "agent", agentId, timestamp);
+    const operation = await result.op();
+
+    if ("group" in hab && hab.group) {
+      const recipients = await this.listOtherMembers(hab);
+      const seal = this.createSeal(hab);
+      const sigers = result.sigs.map((sig: string) => new Siger({ qb64: sig }));
+      const roleims = d(messagize(result.serder, sigers, seal, undefined, undefined, false));
+      const atc = roleims.substring(result.serder.size);
+
+      await this.client.exchanges().send(
+        hab.group.mhab.name,
+        "multisig",
+        hab.group.mhab,
+        "/multisig/rpy",
+        { gid: hab.prefix },
+        {
+          rpy: [result.serder, atc],
+        },
+        recipients
+      );
+    }
+
+    return operation;
+  }
+
+  async refreshState(groupAlias: string, anchor?: string) {
+    let hab = await this.client.identifiers().get(groupAlias);
+    const op = await this.client.keyStates().query(hab.prefix, undefined, anchor);
+    return op;
+  }
+
+  async createRegistry(args: CreateRegistryArgs) {
+    let hab = await this.client.identifiers().get(args.name);
+
+    const result = await this.client.registries().create({
+      name: args.name,
+      registryName: args.registryName,
+      nonce: args.nonce,
+    });
+
+    const op = await result.op();
+
+    if ("group" in hab && hab.group) {
+      const recipients = await this.listOtherMembers(hab);
+      const sigers = result.sigs.map((sig: string) => new Siger({ qb64: sig }));
+      const ims = d(messagize(result.serder, sigers));
+      const atc = ims.substring(result.serder.size);
+
+      await this.client.exchanges().send(
+        hab.group.mhab.name,
+        "multisig",
+        hab.group.mhab,
+        "/multisig/vcp",
+        { gid: hab.prefix },
+        {
+          vcp: [result.regser, ""],
+          anc: [result.serder, atc],
+        },
+        recipients
+      );
+    }
+
+    return op;
+  }
+
+  async wait<T>(
+    op: Operation<T>,
+    options: {
+      signal?: AbortSignal;
+      minSleep?: number;
+      maxSleep?: number;
+      increaseFactor?: number;
+      onRetry?: (op: Operation<T>) => void;
+    } = {}
+  ): Promise<Operation<T>> {
+    let operation = op;
+    let retryCount = 0;
+
+    while (!operation.done) {
+      options.signal?.throwIfAborted();
+
+      operation = await this.client.operations().get(operation.name);
+
+      if (options.onRetry) {
+        options.onRetry(operation);
+      }
+
+      await sleep(
+        Math.min(
+          Math.max(options.minSleep ?? 100, (options.increaseFactor ?? 2) ** retryCount),
+          options.maxSleep ?? 1000
+        )
+      );
+
+      retryCount++;
+    }
+    return operation;
   }
 }
