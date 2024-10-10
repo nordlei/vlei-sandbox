3c239a9569718e854bbdf9295ede2679ab716b32
lenkan <dlenksjo@gmail.com>
2024-10-10 18:05:49 +0200

    update to only use one agent

--- src/issues/multisig-join-late-add-agents.test.ts
+++ src/issues/multisig-join-late-add-agents.test.ts
@@ -64,12 +64,10 @@ test("First two members create multisig group", async () => {
 const dt = createTimestamp();
 
 test("First two members create agent endrole", async () => {
-  const smids = wallets.map((w) => w.identifier.prefix);
-
   await Promise.all(
     [wallet1, wallet2].map(async (wallet) => {
-      const op = await wallet.configureGroupAgents(groupAlias, dt);
-      return Promise.all(op.map((o) => wallet.wait(o, { signal: AbortSignal.timeout(20000) })));
+      const op1 = await wallet.addEndRole(groupAlias, dt, wallet1.client.agent!.pre);
+      await wallet.wait(op1, { signal: AbortSignal.timeout(20000) });
     })
   );
 });
@@ -92,27 +90,18 @@ test("Ensure group AID is the same", async () => {
 // These are just steps to try and force the keria agent to collect the signatures of the multisig group
 // so that member 3 can complete the operation below
 describe.skip("Query", () => {
-  test("First member does multisig query", async () => {
-    const group = await wallet1.client.identifiers().get(groupAlias);
-    await wallet1.queryKeyState(group.prefix, { sn: "0", signal: AbortSignal.timeout(10000) });
-  });
-
-  test("Last member does multisig query", async () => {
-    const group = await wallet3.client.identifiers().get(groupAlias);
-    await wallet3.queryKeyState(group.prefix, { sn: "0", signal: AbortSignal.timeout(10000) });
-  });
-
   test("Last member resolves group OOBI", async () => {
     const oobi = await wallet1.generateOobi(groupAlias);
     await wallet3.resolveOobi(oobi);
   });
 });
 
-test("Last member creates agent endroles", async () => {
-  const op = await wallet3.configureGroupAgents(groupAlias, dt);
-  await Promise.all(
-    op.map((o) =>
-      wallet3.wait(o, { signal: AbortSignal.timeout(20000), onRetry: (op) => console.dir(op, { depth: 100 }) })
-    )
-  );
+test("Last member creates agent endrole", async () => {
+  const op1 = await wallet3.addEndRole(groupAlias, dt, wallet1.client.agent!.pre);
+
+  // Note, this OOBI resolution only works if you make some changes to keria, will report
+  // const oobi = await wallet1.generateOobi(groupAlias);
+  // await wallet3.resolveOobi(oobi);
+
+  await wallet3.wait(op1, { signal: AbortSignal.timeout(20000) });
 });
