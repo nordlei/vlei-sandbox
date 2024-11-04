25e216387650f35a3115cc32b8dda96cdc7057d6
David de Meijere <david.demeijere@nordlei.org>
2024-11-04 19:36:20 +0100

    add test for new qvi member to list old registries

--- src/test-wallet.ts
+++ src/test-wallet.ts
@@ -13,6 +13,7 @@ import {
   CreateRegistryArgs,
   CredentialData,
   Saider,
+  State,
 } from "signify-ts";
 // } from "signify-ts-old"; // Use signify-ts-old if testing against KERIA 0.1.3
 import { sleep } from "./test-utils";
@@ -52,7 +53,7 @@ export class TestWallet {
     return this._client;
   }
 
-  private async refreshIdentifier() {
+  async refreshIdentifier() {
     try {
       const result = await this.client.identifiers().get(this.options.alias);
       this._identifier = result;
@@ -71,11 +72,20 @@ export class TestWallet {
     await ready();
     if (!this.options.passcode) {
       const passcode = randomPasscode();
-      const client = new SignifyClient(KERIA_AGENT_URL, passcode, Tier.low, KERIA_BOOT_URL);
+      const client = new SignifyClient(
+        KERIA_AGENT_URL,
+        passcode,
+        Tier.low,
+        KERIA_BOOT_URL
+      );
       await client.boot();
       this._client = client;
     } else {
-      const client = new SignifyClient(KERIA_AGENT_URL, this.options.passcode, Tier.low);
+      const client = new SignifyClient(
+        KERIA_AGENT_URL,
+        this.options.passcode,
+        Tier.low
+      );
       this._client = client;
     }
   }
@@ -113,7 +123,9 @@ export class TestWallet {
     const inceptOperation = await inceptResult.op();
     await this.wait(inceptOperation);
 
-    const agentResult = await this.client.identifiers().addEndRole(alias, "agent", agentId);
+    const agentResult = await this.client
+      .identifiers()
+      .addEndRole(alias, "agent", agentId);
     const agentOperation = await agentResult.op();
     await this.wait(agentOperation);
 
@@ -127,7 +139,9 @@ export class TestWallet {
   }
 
   async generateOobi(alias?: string): Promise<string> {
-    const result = await this.client.oobis().get(alias ?? this.options.alias, "agent");
+    const result = await this.client
+      .oobis()
+      .get(alias ?? this.options.alias, "agent");
 
     const oobi = result.oobis[0];
 
@@ -143,12 +157,24 @@ export class TestWallet {
     await this.wait(op2);
   }
 
-  async queryKeyState(prefix: string, options: { sn?: string; signal?: AbortSignal } = {}) {
+  async queryKeyState(
+    prefix: string,
+    options: { sn?: string; signal?: AbortSignal } = {}
+  ) {
     const op = await this.client.keyStates().query(prefix, options.sn);
     await this.wait(op, { signal: options.signal });
   }
 
-  async createGroup(groupAlias: string, args: { smids: string[]; isith: number; wits: string[]; toad: number }) {
+  async createGroup(
+    groupAlias: string,
+    args: {
+      smids: string[];
+      isith: number;
+      nsith?: number;
+      wits: string[];
+      toad: number;
+    }
+  ) {
     const mhab = this.identifier;
     if (!mhab) {
       throw new Error("No local identifier created");
@@ -164,7 +190,7 @@ export class TestWallet {
     const res = await this.client.identifiers().create(groupAlias, {
       algo: Algos.group,
       isith: args.isith,
-      nsith: args.isith,
+      nsith: args.nsith ?? args.isith,
       mhab,
       states,
       rstates: states,
@@ -185,7 +211,15 @@ export class TestWallet {
 
     await this.client
       .exchanges()
-      .send(mhab.name, "multisig", mhab, "/multisig/icp", { smids: args.smids }, embeds, args.smids);
+      .send(
+        mhab.name,
+        "multisig",
+        mhab,
+        "/multisig/icp",
+        { smids: args.smids },
+        embeds,
+        args.smids
+      );
 
     return await res.op();
   }
@@ -205,14 +239,20 @@ export class TestWallet {
   }
 
   async listEndRoles(alias: string, role = "agent") {
-    const path = role !== undefined ? `/identifiers/${alias}/endroles/${role}` : `/identifiers/${alias}/endroles`;
+    const path =
+      role !== undefined
+        ? `/identifiers/${alias}/endroles/${role}`
+        : `/identifiers/${alias}/endroles`;
     const response: Response = await this.client.fetch(path, "GET", null);
     if (!response.ok) throw new Error(await response.text());
     const result = await response.json();
     return result;
   }
 
-  async configureGroupAgents(groupAlias: string, dt: string): Promise<Operation[]> {
+  async configureGroupAgents(
+    groupAlias: string,
+    dt: string
+  ): Promise<Operation[]> {
     const members = await this.client.identifiers().members(groupAlias);
     const ops: Operation[] = [];
 
@@ -235,7 +275,9 @@ export class TestWallet {
       .identifiers()
       .members(group.name)
       .then((members) =>
-        members.signing.map((m: { aid: string }) => m.aid).filter((aid: string) => aid !== group.group?.mhab.prefix)
+        members.signing
+          .map((m: { aid: string }) => m.aid)
+          .filter((aid: string) => aid !== group.group?.mhab.prefix)
       );
 
     return recipients;
@@ -243,14 +285,18 @@ export class TestWallet {
 
   async addEndRole(groupAlias: string, timestamp: string, agentId: string) {
     const hab = await this.client.identifiers().get(groupAlias);
-    const result = await this.client.identifiers().addEndRole(hab.name, "agent", agentId, timestamp);
+    const result = await this.client
+      .identifiers()
+      .addEndRole(hab.name, "agent", agentId, timestamp);
     const operation = await result.op();
 
     if ("group" in hab && hab.group) {
       const recipients = await this.listOtherMembers(hab);
       const seal = this.createSeal(hab);
       const sigers = result.sigs.map((sig: string) => new Siger({ qb64: sig }));
-      const roleims = d(messagize(result.serder, sigers, seal, undefined, undefined, false));
+      const roleims = d(
+        messagize(result.serder, sigers, seal, undefined, undefined, false)
+      );
       const atc = roleims.substring(result.serder.size);
 
       await this.client.exchanges().send(
@@ -271,7 +317,9 @@ export class TestWallet {
 
   async refreshState(groupAlias: string, anchor?: string) {
     let hab = await this.client.identifiers().get(groupAlias);
-    const op = await this.client.keyStates().query(hab.prefix, undefined, anchor);
+    const op = await this.client
+      .keyStates()
+      .query(hab.prefix, undefined, anchor);
     return op;
   }
 
@@ -318,7 +366,8 @@ export class TestWallet {
     if ("group" in hab && hab.group) {
       const recipients = await this.listOtherMembers(hab);
       const keeper = this.client.manager?.get(hab);
-      const sigs = (await keeper?.sign(new TextEncoder().encode(result.anc.raw))) ?? [];
+      const sigs =
+        (await keeper?.sign(new TextEncoder().encode(result.anc.raw))) ?? [];
       const sigers = sigs.map((sig: string) => new Siger({ qb64: sig }));
       const ims = d(messagize(result.anc, sigers));
       const atc = ims.substring(result.anc.size);
@@ -344,7 +393,11 @@ export class TestWallet {
   async clearNotifications() {
     let { notes } = await this.client.notifications().list();
     while (notes.length > 0) {
-      await Promise.all(notes.map((note: { i: string }) => this.client.notifications().delete(note.i)));
+      await Promise.all(
+        notes.map((note: { i: string }) =>
+          this.client.notifications().delete(note.i)
+        )
+      );
 
       const response = await this.client.notifications().list();
       notes = response.notes;
@@ -368,7 +421,9 @@ export class TestWallet {
   async waitNotification(route: string, signal: AbortSignal) {
     while (!signal.aborted) {
       const response = await this.client.notifications().list();
-      const note = response.notes.reverse().find((note: { a: { r: string } }) => note.a.r === route);
+      const note = response.notes
+        .reverse()
+        .find((note: { a: { r: string } }) => note.a.r === route);
 
       if (note) {
         return note;
@@ -376,6 +431,120 @@ export class TestWallet {
     }
 
     signal.throwIfAborted();
+  }
+
+  async queryLastKeyState(prefix: string, sn?: number): Promise<State> {
+    const { oobi, alias } = await this.client.contacts().get(prefix);
+    await this.resolveOobi(oobi, alias);
+    const operation = await this.client
+      .keyStates()
+      .query(prefix, sn ? `${sn}` : undefined);
+    const result = await this.wait<State>(operation);
+    if (!result.response) {
+      throw new Error("no response from queryLastKeyState");
+    }
+    return result.response;
+  }
+
+  async rotateIdentifier(props: RotateIdentifierProps) {
+    if (!this._identifier) {
+      throw new Error("identifier not set");
+    }
+
+    await this.refreshIdentifier();
+
+    const localIdentifier = this._identifier;
+
+    const { identifierAlias, rotationStates } = props;
+
+    const identifierToRotate = await this.client
+      .identifiers()
+      .get(identifierAlias);
+
+    if (!identifierToRotate.group) {
+      const result = await this.client.identifiers().rotate(identifierAlias);
+      return result.op();
+    }
+
+    if (!rotationStates) {
+      throw new Error("must provide rotationStates for group identifier");
+    }
+
+    const members = await this.getGroupMembers({ alias: identifierAlias });
+    const membersAfterRotation = members.rotation.map(({ aid }) => aid);
+
+    const signingStates = await Promise.all(
+      membersAfterRotation.map(async (prefix) => {
+        if (prefix === localIdentifier.prefix) {
+          return localIdentifier.state;
+        }
+        return this.queryLastKeyState(prefix);
+      })
+    );
+    const rotationStatesQueried = await Promise.all(
+      rotationStates.map(async (state) => {
+        if (state.i === localIdentifier.prefix) {
+          return localIdentifier.state;
+        }
+        return this.queryLastKeyState(state.i);
+      })
+    );
+
+    const result = await this.client.identifiers().rotate(identifierAlias, {
+      states: signingStates,
+      rstates: rotationStatesQueried,
+    });
+
+    const operation = await result.op();
+
+    const attachment = d(
+      messagize(
+        result.serder,
+        result.sigs.map((sig: string) => new Siger({ qb64: sig }))
+      )
+    ).substring(result.serder.size);
+
+    const smids = signingStates.map((states) => states.i);
+    const rmids = rotationStatesQueried.map((states) => states.i);
+
+    const recipients = [...smids, ...rmids]
+      .filter((aid) => aid !== localIdentifier.prefix)
+      .filter((aid, i, states) => states.indexOf(aid) === i);
+
+    await this.client.exchanges().send(
+      identifierToRotate.group.mhab.name,
+      "multisig",
+      identifierToRotate.group.mhab,
+      "/multisig/rot",
+      { gid: result.serder.pre, smids, rmids },
+      {
+        rot: [result.serder, attachment],
+      },
+      recipients ?? []
+    );
+
+    return operation;
+  }
+
+  async getGroupMembers(args: { alias: string }): Promise<GroupMembers> {
+    const response = await this.client.fetch(
+      `/identifiers/${args.alias}/members`,
+      "GET",
+      null
+    );
+    if (!response.ok) {
+      throw new Error(
+        `HTTP ${response.status} ${
+          response.statusText
+        } - ${await response.text()}`
+      );
+    }
+    const groupMembers = await response.json();
+    if (!groupMembers) {
+      return { rotation: [], signing: [] };
+    }
+
+    return groupMembers as any;
   }
 
   async wait<T>(
@@ -405,7 +574,10 @@ export class TestWallet {
 
       await sleep(
         Math.min(
-          Math.max(options.minSleep ?? 100, (options.increaseFactor ?? 2) ** retryCount),
+          Math.max(
+            options.minSleep ?? 100,
+            (options.increaseFactor ?? 2) ** retryCount
+          ),
           options.maxSleep ?? 1000
         )
       );
@@ -415,12 +587,30 @@ export class TestWallet {
     return operation;
   }
 
-  async getRegistry(args: { owner: string; name: string }): Promise<{ regk: string }> {
+  async getRegistry(args: {
+    owner: string;
+    name: string;
+  }): Promise<{ regk: string }> {
     const path = `/identifiers/${args.owner}/registries/${args.name}`;
     const method = "GET";
     const res = await this.client.fetch(path, method, null);
     return (await res.json()) as any;
   }
+}
+
+export interface RotateIdentifierProps {
+  identifierAlias: string;
+  rotationStates?: State[];
+}
+
+export interface GroupMember {
+  aid: string;
+  ends: Record<string, unknown>;
+}
+
+export interface GroupMembers {
+  signing: GroupMember[];
+  rotation: GroupMember[];
 }
 
 export interface CredentialConfig {
